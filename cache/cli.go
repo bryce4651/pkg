54bbf3f0626a346bf6056d7422f7eeaf50a29918
bryce <you@example.com>
2024-02-16 14:50:37 +0800

    fix: ok

--- cache/cli.go
+++ cache/cli.go
@@ -8,11 +8,11 @@ import (
 )
 
 type CacheCfg struct {
-	ClientName string `json:"client_name" yaml:"client_name" env:"CACHE_CLIENT_NAME"`
-	Address    string `json:"address" yaml:"address" env:"CACHE_ADDRESS"`
-	Username   string `json:"username" yaml:"username" env:"CACHE_USERNAME"`
-	Password   string `json:"password" yaml:"password" env:"CACHE_PASSWORD"`
-	DB         int    `json:"db" yaml:"db" env:"CACHE_DB"`
+	ClientName string `json:"client_name" yaml:"client_name" env:"name=CACHE_CLIENT_NAME"`
+	Address    string `json:"address" yaml:"address" env:"name=CACHE_ADDRESS"`
+	Username   string `json:"username" yaml:"username" env:"name=CACHE_USERNAME"`
+	Password   string `json:"password" yaml:"password" env:"name=CACHE_PASSWORD"`
+	DB         int    `json:"db" yaml:"db" env:"name=CACHE_DB"`
 }
 
 func GetCacheCli(config *CacheCfg) (*redis.Client, error) {
